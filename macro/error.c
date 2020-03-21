ac04c9574ce14a72aca13a9fec50e18ca9d186c4
zhuxinran <zhuran@mail.ustc.edu.cn>
2020-03-21 09:53:31 +0800

    error

---
+++ macro/error.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#ifndef __cplusplus
+	#error This file should be processed with C++ compiler.
+#endif
+class CppClass
+{
+private:
+	int m_value;
+public:
+	CppClass()
+	{
+	}
+	~CppClass()	
+	{
+	}
+};
+int main()
+{
+	return 0;	
+}
